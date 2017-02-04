bdf4ff2dca5532a379040c705bae52659784d6f8
Yunpeng Gao <yg6c2@mst.edu>
2017-02-04 14:50:46 -0600

    testing

--- src/application/MainModel.java
+++ src/application/MainModel.java
@@ -1,7 +1,7 @@
 package application;
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
+//import static org.junit.Assert.assertEquals;
+//
+//import org.junit.Test;
 
 public class MainModel {
 	private int calValue;
