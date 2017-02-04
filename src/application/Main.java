d6c0cb4a95fa91745e55e07824bf6f7632661567
Yunpeng Gao <yg6c2@mst.edu>
2017-02-04 15:28:13 -0600

    fix some problems

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,28 +3,17 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-<<<<<<< HEAD
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
-=======
-import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
->>>>>>> bdf4ff2dca5532a379040c705bae52659784d6f8
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-<<<<<<< HEAD
 			Pane root = new MainView();
 			Scene scene = new Scene(root,400,400);
 			
-=======
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("MainView.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
->>>>>>> bdf4ff2dca5532a379040c705bae52659784d6f8
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- src/application/MainVC.java
+++ src/application/MainVC.java
@@ -2,11 +2,8 @@ package application;
 
 public class MainVC {
 	
-<<<<<<< HEAD
 	public static void actionOnPressed(){
 		System.out.println("btn pressed");
 	}
 	
-=======
->>>>>>> bdf4ff2dca5532a379040c705bae52659784d6f8
 }
